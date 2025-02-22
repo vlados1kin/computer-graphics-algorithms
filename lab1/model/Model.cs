46628f4d5c755ab3619181db0ff6cec8524d5137
ekaterinapolitykina <eka.politykina@gmail.com>
2025-02-22 23:16:40 +0300

    added lab1 task1

---
+++ lab1/model/Model.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1.model
+{
+    public class Model
+    {
+        private float _scale;
+        private Vector3 _translation = Vector3.Zero;
+        private Vector3 _rotation = Vector3.Zero;
+
+        public List<Vector4> OriginalVertices { get; } = [];
+
+        public Vector4[] TransformedVertices { get; set; } = [];
+
+        // vt u [v] [w]
+        public List<Vector3> TextureCoords { get; } = [];
+        // vn i j k
+        public List<Vector3> Normals { get; } = [];
+
+        public List<Face> Faces { get; } = [];
+    }
+}
---
+++ lab1/parser/ObjParser.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using lab1.model;
+
+
+namespace lab1.parser
+{
+    public static class ObjParser
+    {
+
+        public static Model Load(string filePath)
+        {
+            var model = new Model();
+            foreach (var line in File.ReadLines(filePath))
+            {
+                var parts = line.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                if (parts.Length == 0 || parts[0].StartsWith("#")) continue;
+
+                switch (parts[0])
+                {
+                    //Вершина
+                    case "v":
+                        model.OriginalVertices.Add(ParseVector4(parts.Skip(1).ToArray()));
+                        break;
+                    // Текстурная координата
+                    case "vt":
+                        model.TextureCoords.Add(ParseVector3(parts.Skip(1).ToArray()));
+                        break;
+                    // Нормаль
+                    case "vn":
+                        model.Normals.Add(ParseVector3(parts.Skip(1).ToArray()));
+                        break;
+                    // Грань
+                    case "f":
+                        model.Faces.Add(ParseFace(parts.Skip(1).ToArray()));
+                        break;
+                }
+            }
+            return model;
+        }
+
+        private static Vector4 ParseVector4(string[] parts)
+        {
+            float x = float.Parse(parts[0], CultureInfo.InvariantCulture);
+            float y = parts.Length > 1 ? float.Parse(parts[1], CultureInfo.InvariantCulture) : 0;
+            float z = parts.Length > 2 ? float.Parse(parts[2], CultureInfo.InvariantCulture) : 0;
+            float w = parts.Length > 3 ? float.Parse(parts[3], CultureInfo.InvariantCulture) : 1;
+            return new Vector4(x, y, z, w);
+        }
+
+        private static Vector3 ParseVector3(string[] parts)
+        {
+            float u = float.Parse(parts[0], CultureInfo.InvariantCulture);
+            float v = parts.Length > 1 ? float.Parse(parts[1], CultureInfo.InvariantCulture) : 0;
+            float w = parts.Length > 2 ? float.Parse(parts[2], CultureInfo.InvariantCulture) : 0;
+            return new Vector3(u, v, w);
+        }
+        private static Face ParseFace(string[] parts)
+        {
+            var face = new Face();
+            foreach (var part in parts)
+            {
+                var indices = part.Split('/');
+                int v = int.Parse(indices[0]);
+                int vt = indices.Length > 1 && indices[1] != "" ? int.Parse(indices[1]) : -1;
+                int vn = indices.Length > 2 ? int.Parse(indices[2]) : -1;
+                face.Vertices.Add(new FaceVertex(v, vt, vn));
+            }
+            return face;
+        }
+    }
+
+}
